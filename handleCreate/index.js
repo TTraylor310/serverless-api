921b978a1032ca840e5d51cf0dc74c6529ba6493
Tim Traylor <ttraylor83@gmail.com>
2022-10-13 08:28:50 -0700

    typo error

--- handleCreate/index.js
+++ handleCreate/index.js
@@ -1,29 +1,19 @@
 const dynamoose = require('dynamoose');
 
-
-const friendSchema = new dynamoose.Schema({
+const peoplesSchema = new dynamoose.Schema({
     id: String,
     name: String,
     phone: String,
 });
 
-
 const peoplesModel = dynamoose.model('peoples', peoplesSchema);
 
-
 exports.handler = async (event) => {
-    console.log('Welcome to the World!', event.body)
+    // console.log('', event.body)
 
-    // plan A extract from the body
     let parsedBody = JSON.parse(event.body);
     let { id, name, phone} = parsedBody;
-
-    // plan B if cannot extract from the body
-    // let { id, name, phone} = event.queryStringParameters;
-
     let peoples = {id, name, phone}
-    console.log('CONSOLE LOGS-------', peoples);
-
     const response = {statusCode: null, body: null};
 
     try {
