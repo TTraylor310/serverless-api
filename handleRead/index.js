9fe747c78652b57ab4ec7525e442a1a413395253
Tim Traylor <ttraylor83@gmail.com>
2022-10-13 08:45:39 -0700

    fixed POST method (handCreate) and added the gitignores

--- handleRead/index.js
+++ handleRead/index.js
@@ -1,15 +1,12 @@
 const dynamoose = require('dynamoose');
 
-
 const peoplesSchema = new dynamoose.Schema({
     id: String,
     name: String,
     phone: String,
 });
 
-
 const peoplesModel = dynamoose.model('peoples', peoplesSchema);
-
 
 exports.handler = async (event) => {
 
