61851072daa6f683ae157db85ae45bd97b8a9838
Tim Traylor <ttraylor83@gmail.com>
2022-10-13 07:55:43 -0700

    read framework set up

--- handleRead/index.js
+++ handleRead/index.js
@@ -1,40 +1,28 @@
 const dynamoose = require('dynamoose');
 
-// const friendSchema = new dynamoose.Schema({
-//     id: String,
-//     name: String,
-//     phone: String,
-// });
+const peoplesSchema = new dynamoose.Schema({
+    id: String,
+    name: String,
+    phone: String,
+});
 
-// const friendModel = dynamoose.model('friends-demo', friendSchema);
-
-// exports.handler = async (event) => {
-    
-//     console.log('---------', event.queryStringParameters);
-//     const response = {statusCode: null, body: null};
-
-//     try {
-//         let friendRecords = await friendModel.scan().exec();
-//         response.statusCode = 200;
-//         response.body = JSON.stringify(friendRecords);
-
-//     } catch (e) {
-//         console.log(e);
-//         response.statusCode = 500;
-//         response.body = JSON.stringify(e.message);
-//     }
-
-//     return response;
-// };
-
+const peoplesModel = dynamoose.model('Peoples Model', peoplesSchema);
 
 exports.handler = async (event) => {
     
-  console.log('Testing the Test:', event.queryStringParameters);
-  
-  const response = {
-      statusCode: 200,
-      body: JSON.stringify('Check MY STUFFs'),
-  };
-  return response;
+    console.log('---------', event.queryStringParameters);
+    const response = {statusCode: null, body: null};
+
+    try {
+        let peoplesRecords = await peoplesModel.scan().exec();
+        response.statusCode = 200;
+        response.body = JSON.stringify(peoplesRecords);
+
+    } catch (e) {
+        console.log(e);
+        response.statusCode = 500;
+        response.body = JSON.stringify(e.message);
+    }
+
+    return response;
 };
