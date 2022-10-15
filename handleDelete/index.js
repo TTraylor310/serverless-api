1a957a918158abab2a5117eb614c4632dbb119ad
Tim Traylor <107095841+TTraylor310@users.noreply.github.com>
2022-10-15 16:46:12 -0700

    updated handleDelete for Lab18

---
+++ handleDelete/index.js
@@ -0,0 +1,28 @@
+'use strict'
+const dynamoose = require('dynamoose');
+
+const peoplesSchema = new dynamoose.Schema({
+  id: String,
+  name: String,
+  phone: String,
+});
+
+const peoplesModel = dynamoose.model('peoples', peoplesSchema);
+
+exports.handler = async (event) => {
+
+  let id = event?.pathParameters?.id;
+  const response = { statusCode: null, body: null };
+  try {
+    let deletedItem = await friendModel.delete(id);
+    response.statusCode = 200;
+    response.body = JSON.stringify({message: 'success', itemDeleted: deletedItem});
+
+  }catch (e){
+    console.log(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify(e.message);
+  }
+
+  return response;
+};
